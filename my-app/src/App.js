b7c37086db9976e94880bd9a9274db97c1254344
seigfred0 <saysonseigfred@gmail.com>
2023-12-13 22:11:04 +0800

    update

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+// import { useState } from 'react';
+
+
+const App = () =>  {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+   alert('counter being changed')
+  }, [counter])
+
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>  
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+    </div>
+  );
+}
+
+export default App;
