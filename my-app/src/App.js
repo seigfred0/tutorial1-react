73410da51122456fbafdf1d079a3cf67f8fd3ca4
seigfred0 <saysonseigfred@gmail.com>
2023-12-14 08:19:51 +0800

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,54 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-// import { useState } from 'react';
+import React, { useEffect } from "react";
+import './app.css';
 
+// 6eec4094
 
-const App = () =>  {
-  const [counter, setCounter] = useState(0);
+const API_URL = 'http://www.omdbapi.com?apikey=6eec4094';
 
-  useEffect(() => {
-   alert('counter being changed')
-  }, [counter])
-
-  return (
-    <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>  
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
-    </div>
-  );
+const movie1 = {
+    "Title": "Superman, Spiderman or Batman",
+    "Year": "2011",
+    "imdbID": "tt2084949",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
 }
+
+const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search)
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman')
+    }, [])
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input placeholder="Search Movies" value="Superman" onChange={() => {}}/>
+                <img src="{SearchIcon}" alt="Search" onClick={() => {}}/>
+            </div>
+
+            <div className="container">
+                <div className="movie">
+                    <div>
+                        <p>{movie1.Year}</p>
+                    </div>
+
+                    <div>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.title}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
 
 export default App;
